ab9d5b7aae2846e5c245e7cbe06683fbe29f52f4
Matt Melquiond <matt.llvw@gmail.com>
2019-05-18 00:06:58 +0200

    added ci_script.sh, fixed makefile

---
+++ docker/utils/ci_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while ! [[ $(docker-compose logs | grep "The Salt Master has cached the public key for this node") ]];
+do
+    echo "Waiting Salt Master..."
+    sleep 20
+done
+docker-compose exec web pip install --user -r requirements/test
+make tests
