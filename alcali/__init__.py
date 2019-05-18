9394faae49bed247367d1ba95b11ee5c2b382556
Nicolas Rodriguez <nicoladmin@free.fr>
2019-05-19 01:54:24 +0200

    fix(env): improve env vars loading, add gunicorn

--- alcali/__init__.py
+++ alcali/__init__.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
 
+# Get Django environment
 DJANGO_SETTINGS = os.environ.get('DJANGO_SETTINGS', 'alcali.config.settings.dev')
 
-
 def manage():
+    # This file is copied to ~/.pyenv/versions/3.7.3/lib/python3.7/site-packages
+    # during pip install, so use getcwd() to determine .env location
+    file = os.path.join(os.getcwd(), '.env')
+    dotenv.read_dotenv(file)
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', DJANGO_SETTINGS)
 
     try:
--- alcali/config/wsgi.py
+++ alcali/config/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+import dotenv
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alcali.config.settings')
+# Get Django environment
+DJANGO_SETTINGS = os.environ.get('DJANGO_SETTINGS', 'alcali.config.settings.dev')
+
+# This file is copied to ~/.pyenv/versions/3.7.3/lib/python3.7/site-packages
+# during pip install, so use getcwd() to determine .env location
+file = os.path.join(os.getcwd(), '.env')
+dotenv.read_dotenv(file)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', DJANGO_SETTINGS)
 
 application = get_wsgi_application()
