21d702b70cbf28cb7af68e97f09d14bb4f17f202
Matt Melquiond <matt.llvw@gmail.com>
2019-08-15 11:26:17 +0200

    bumped deps, fixed datatables order

--- alcali/web/static/js/src/minion_list.js
+++ alcali/web/static/js/src/minion_list.js
@@ -57,7 +57,6 @@ let minionColDef = [
 let minionTable;
 function createMinionTable() {
   minionTable = $('.js-exportable').DataTable({
-    "order": [[0, "desc"]],
     dom: "<'row'<'col-sm-6'l><'col-sm-6'f>>" +
       "<'row'<'col-sm-12'tr>>" +
       "<'row'<'col-sm-2'i><'col-sm-3 pull-left'B><'col-sm-7'p>>",
