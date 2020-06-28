c8fa39468e4a3b2957d648f872d26d90a49697f3
Matt Melquiond <matt.llvw@gmail.com>
2020-06-28 17:30:51 +0200

    offline version

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,3 +1,5 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'typeface-roboto/index.css'
 import Vue from "vue"
 import Vuetify, { VSnackbar, VBtn, VIcon } from "vuetify/lib"
 
