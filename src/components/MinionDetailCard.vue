ea73daa763a62f251d65fd2f32154f6f9a2597f6
Lionel MORIN <lionel.morin@ac-dijon.fr>
2021-06-04 18:24:20 +0200

    Fix MinionDetailCard

--- src/components/MinionDetailCard.vue
+++ src/components/MinionDetailCard.vue
@@ -1,43 +1,45 @@
 <template>
   <v-container fluid>
     <v-card>
-      <v-tabs
-          v-model="tab"
-      >
+      <v-tabs v-model="tab">
         <v-tabs-slider></v-tabs-slider>
 
         <v-tab href="#grain">
-          {{$t('components.MinionDetailCard.Grains')}}
+          {{ $t("components.MinionDetailCard.Grains") }}
         </v-tab>
 
         <v-tab href="#pillar">
-          {{$t('components.MinionDetailCard.Pillar')}}
+          {{ $t("components.MinionDetailCard.Pillar") }}
         </v-tab>
         <v-tab href="#history">
-          {{$t('components.MinionDetailCard.History')}}
+          {{ $t("components.MinionDetailCard.History") }}
         </v-tab>
         <v-tab href="#graph">
-          {{$t('components.MinionDetailCard.Graph')}}
+          {{ $t("components.MinionDetailCard.Graph") }}
         </v-tab>
         <v-tab v-for="field in minion.custom_fields" v-bind:key="field.name">
-          {{field.name}}
+          {{ field.name }}
         </v-tab>
       </v-tabs>
       <v-tabs-items v-model="tab">
         <v-tab-item id="grain">
           <div class="text-right">
-            <v-btn @click="fold('grainCm')" class="overlayedBtn">{{ grainCmFolded ? `${$t('components.MinionDetailCard.unfold')}` : `${$t('components.MinionDetailCard.fold')}`}}</v-btn>
+            <v-btn @click="fold('grainCm')" class="overlayedBtn">{{
+              grainCmFolded ? $t("components.MinionDetailCard.Unfold") : $t("components.MinionDetailCard.Fold")
+            }}</v-btn>
           </div>
           <codemirror v-model="code" ref="grainCm" :options="cmOptions"></codemirror>
         </v-tab-item>
         <v-tab-item id="pillar">
           <div class="text-right">
-            <v-btn @click="fold('pillarCm')" class="overlayedBtn">{{ pillarCmFolded ? `${$t('components.MinionDetailCard.unfold')}` : `${$t('components.MinionDetailCard.fold')}`}}</v-btn>
+            <v-btn @click="fold('pillarCm')" class="overlayedBtn">{{
+              pillarCmFolded ? $t("components.MinionDetailCard.Unfold") : $t("components.MinionDetailCard.Fold")
+            }}</v-btn>
           </div>
           <codemirror v-model="codepillar" ref="pillarCm" :options="cmOptions"></codemirror>
         </v-tab-item>
         <v-tab-item id="history">
-          <JobsTable :filter="{'target[]': minion.minion_id}"></JobsTable>
+          <JobsTable :filter="{ 'target[]': minion.minion_id }"></JobsTable>
         </v-tab-item>
         <v-tab-item id="graph" eager>
           <JobsChartCard v-if="minion" :minion="minion.minion_id"></JobsChartCard>
@@ -51,81 +53,79 @@
 </template>
 
 <script>
+// require component
+import CodeMirror from "codemirror";
+import { codemirror } from "vue-codemirror";
 
-  // require component
-  import CodeMirror from "codemirror"
-  import { codemirror } from "vue-codemirror"
+import "codemirror/addon/display/autorefresh.js";
+import "codemirror/addon/fold/foldcode.js";
+import "codemirror/addon/fold/brace-fold.js";
+import "codemirror/addon/fold/foldgutter.js";
+import "codemirror/addon/fold/indent-fold.js";
+import "codemirror/mode/javascript/javascript.js";
 
-  import "codemirror/addon/display/autorefresh.js"
-  import "codemirror/addon/fold/foldcode.js"
-  import "codemirror/addon/fold/brace-fold.js"
-  import "codemirror/addon/fold/foldgutter.js"
-  import "codemirror/addon/fold/indent-fold.js"
-  import "codemirror/mode/javascript/javascript.js"
+// require styles
+import "codemirror/lib/codemirror.css";
+// language js
+import "codemirror/mode/yaml/yaml.js";
+// theme css
+import "../assets/css/made-of-code.css";
 
-  // require styles
-  import "codemirror/lib/codemirror.css"
-  // language js
-  import "codemirror/mode/yaml/yaml.js"
-  // theme css
-  import "../assets/css/made-of-code.css"
+import yaml from "js-yaml";
+import JobsChartCard from "./JobsChartCard";
+import JobsTable from "./JobsTable";
 
-  import yaml from "js-yaml"
-  import JobsChartCard from "./JobsChartCard"
-  import JobsTable from "./JobsTable"
-
-  export default {
-    name: "MinionDetailCard",
-    components: {
-      JobsTable,
-      JobsChartCard,
-      codemirror,
+export default {
+  name: "MinionDetailCard",
+  components: {
+    JobsTable,
+    JobsChartCard,
+    codemirror,
+  },
+  data() {
+    return {
+      tab: null,
+      code: yaml.safeDump(JSON.parse(this.minion.grain)),
+      codepillar: yaml.safeDump(JSON.parse(this.minion.pillar)),
+      grainCmFolded: false,
+      pillarCmFolded: false,
+      cmOptions: {
+        tabSize: 4,
+        mode: "yaml",
+        theme: "made-of-code",
+        line: true,
+        autoRefresh: true,
+        lineNumbers: false,
+        readOnly: true,
+        cursorBlinkRate: 0,
+        //viewportMargin: Infinity,
+        foldGutter: true,
+        gutters: ["CodeMirror-foldgutter"],
+      },
+    };
+  },
+  methods: {
+    yamlRepr(data) {
+      return yaml.safeDump(JSON.parse(data));
     },
-    data() {
-      return {
-        tab: null,
-        code: yaml.safeDump(JSON.parse(this.minion.grain)),
-        codepillar: yaml.safeDump(JSON.parse(this.minion.pillar)),
-        grainCmFolded: false,
-        pillarCmFolded: false,
-        cmOptions: {
-          tabSize: 4,
-          mode: "yaml",
-          theme: "made-of-code",
-          line: true,
-          autoRefresh: true,
-          lineNumbers: false,
-          readOnly: true,
-          cursorBlinkRate: 0,
-          //viewportMargin: Infinity,
-          foldGutter: true,
-          gutters: ["CodeMirror-foldgutter"],
-        },
+    fold(ref) {
+      if (this[ref + "Folded"] === true) {
+        CodeMirror.commands.unfoldAll(this.$refs[ref].codemirror);
+        this[ref + "Folded"] = false;
+      } else {
+        CodeMirror.commands.foldAll(this.$refs[ref].codemirror);
+        this[ref + "Folded"] = true;
       }
     },
-    methods: {
-      yamlRepr(data) {
-        return yaml.safeDump(JSON.parse(data))
-      },
-      fold(ref) {
-        if (this[ref+'Folded'] === true) {
-          CodeMirror.commands.unfoldAll(this.$refs[ref].codemirror)
-          this[ref+'Folded'] = false
-        } else {
-          CodeMirror.commands.foldAll(this.$refs[ref].codemirror)
-          this[ref+'Folded'] = true
-        }
-      },
-    },
-    props: ["minion"],
-  }
+  },
+  props: ["minion"],
+};
 </script>
 
 <style scoped>
-  .overlayedBtn {
-    position: absolute;
-    right: 0;
-    z-index: 1;
-  }
-
+.overlayedBtn {
+  position: absolute;
+  right: 0;
+  z-index: 1;
+}
 </style>
