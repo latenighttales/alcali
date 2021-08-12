c2465386182bde17b42bf32dcd803b1bbdae2410
Thomas Merkel <thomas.merkel@ionos.com>
2021-08-12 08:47:15 +0200

    Fix Run CLI double prompt output after command
    The ~$ prompt is shown two times after the command run. This fix this
    issue and provide only one prompt.

--- src/components/TerminalCard.vue
+++ src/components/TerminalCard.vue
@@ -75,7 +75,8 @@
               this.$toast("Running " + input)
               this.$http.post("api/run/", formData).then(response => {
                 localEcho.println(response.data.results)
-              }).then(() => readLine()).catch((error) => {
+                console.log(response.data.results)
+              }).then().catch((error) => {
                 this.$toast.error(error.response.data)
               }).then(() => readLine())
             } else {
