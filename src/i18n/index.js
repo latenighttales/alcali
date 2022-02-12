56ab663b17a2c05237a0db5433b5f681936a9a9b
Oliver <gushedaoren@gmail.com>
2022-02-12 20:19:02 +0800

    add chinese (#401)
    Co-authored-by: nixuchen <nixuchen@laiyifen.com>

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,9 +1,10 @@
 import en from "./en.json";
 import fr from "./fr.json";
-
+import zh from "./zh.json";
 export const defaultLocale = "en";
 
 export const languages = {
   en: en,
   fr: fr,
+  zh: zh,
 };
