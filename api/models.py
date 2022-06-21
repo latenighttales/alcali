d5bc299e8fa787f370c2471b0da3416a86b46656
Jamie (Bear) Murphy <1613241+ITJamie@users.noreply.github.com>
2022-06-21 14:24:08 +0100

    Update models.py

--- api/models.py
+++ api/models.py
@@ -145,7 +145,7 @@ class Minions(models.Model):
         # Get all potential jobs.
         states = SaltReturns.objects.filter(
             Q(fun="state.apply") | Q(fun="state.highstate"), id=self.minion_id
-        ).order_by('jid')[0:2]
+        ).order_by('-jid')[0:2]
         states = sorted(states, key=lambda x: x.jid)
 
         # Remove jobs with arguments.
