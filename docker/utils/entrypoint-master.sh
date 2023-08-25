2b53b768cfff84a333878131f8923a72e6e86a9c
Anton Griev <anton@griev.ru>
2023-08-25 17:16:11 +0200

    fix for https://github.com/latenighttales/alcali/issues/500

--- docker/utils/entrypoint-master.sh
+++ docker/utils/entrypoint-master.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+mkdir /var/cache/salt/master
+chown -R salt:salt /var/cache/salt
+
+# mkdir /var/cache/salt/master/extmods
+# mkdir /var/cache/salt/master/roots
+salt-run saltutil.sync_all
 # Wait for web
 echo "Waiting for web"
 ./wait-for -t 60 web:8000
